46657a9cde05fc4236cd494c78bd05ab8092a294
minnje97 <75577850+minnje97@users.noreply.github.com>
2021-03-05 00:12:22 +0900

    css-update

--- jspractice/todo.js
+++ jspractice/todo.js
@@ -27,7 +27,7 @@ function paintToDo(text){
     const delBtn = document.createElement("button");
     const span = document.createElement("span");
     const newId = toDos.length + 1;
-    delBtn.innerText = "📌";
+    delBtn.innerText = "✔";
     delBtn.addEventListener("click", deleteToDo);
     span.innerText=text
     li.appendChild(delBtn);
--- jspractice/weather.js
+++ jspractice/weather.js
@@ -11,7 +11,7 @@ function getWeather(lat, lng){
         }).then(function(json){
         const temperature = json.main.temp;
         const place = json.name;
-        weather.innerText = `${temperature}°     ${place}`;
+        weather.innerText = `${temperature} @ ${place}`;
     });
 }
 
